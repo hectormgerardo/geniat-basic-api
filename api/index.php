f4b247f76aa8ab90887177447131ef9f202cfa98
Hector M Gerardo <hectormgerardo@gmail.com>
2021-10-10 16:07:38 -0700

    docs

--- api/index.php
+++ api/index.php
@@ -1,4 +1,6 @@
 <?php
 	echo "howdy!";
-	// print_r(getenv());
+	echo "<a href='https://raw.githubusercontent.com/hectormgerardo/geniat-basic-api/main/docs/geniat-basic-api.postman_collection.json'>postman collection</a>";
+
+
 ?>
