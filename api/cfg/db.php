4a15836c3d36fcff5c21593834ea5d3d7be1e4f3
Hector M Gerardo <hectormgerardo@gmail.com>
2021-10-10 11:20:06 -0700

    changed db

--- api/cfg/db.php
+++ api/cfg/db.php
@@ -1,11 +1,14 @@
 <?php
 	class DBService{
 		
-		private $host = '127.0.0.1';
+		// private $host = '127.0.0.1';
 		private $name = 'heroku_f0c953e58cdf1ba';
-		private $user = 'root';
-		private $pass = 'SCHAFFHAUSEN';
+		// private $user = 'root';
+		// private $pass = 'SCHAFFHAUSEN';
 
+		private $host = 'us-cdbr-east-04.cleardb.com';
+		private $user = 'b1a8870ee03ea9';
+		private $pass = '75fe4413';
 		private $conn;
 
 		public function getConnection(){
